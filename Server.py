ca021da6061edb84b34fd7d257f13299e7342f98
Unknown <epaglier@purdue.edu>
2017-09-09 19:12:41 -0400

    Server corrections

--- Server.py
+++ Server.py
@@ -7,20 +7,22 @@ listOfZones = list(soco.discover())
 
 def printZones():
     for i in range(len(listOfZones)):
-        print listOfZones[i].name
+        print listOfZones[i].player_name
 
 #Main determines what mode to run in
 def main(argv=sys.argv):
+    print argv[1] #debug
     try:
         if argv[1] == "next": #play next file
             play_next()
-
         elif argv[1] == "list":
             printZones()
         elif argv[1] == "pause":
             pause()
+        else:
+            print "Run with commands next, list, or pause"
     except:
-        print "Run with commands next, list, or pause"
+        print "Failed"
 
 def pause():
     for i in range(len(listOfZones)):
